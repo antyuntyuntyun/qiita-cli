94b0af4cd716a51189e32b8f2322997b92d4cc5c
taptappun <keep_slimbody@yahoo.co.jp>
2022-05-15 15:49:45 +0900

    Refactor/use commander (#34)
    * add Commander library
    
    (cherry picked from commit 8a4b1c509c4b99f2d3647c93d71d8ffc3b464750)
    
    * chnage to commander library style
    
    (cherry picked from commit 301c72b2eecd7fa4f1b1d7ee4fb922be4ed72a35)
    
    * 元々設定されていたメッセージの内容を踏襲してcommanderの場合に合わせた形に適用した
    
    * set usage
    
    * use join splited.

--- src/index.ts
+++ src/index.ts
@@ -6,75 +6,84 @@ import { pullArticle } from './pullArticle';
 import packageJson from '../package.json';
 import { postArticle } from './postArticle';
 import { patchArticle } from './patchArticle';
+import { program } from 'commander';
 
-type CommandType =
-  | string
-  | 'init'
-  | 'pull:article'
-  | 'new:article'
-  | 'post:article'
-  | 'patch:article'
-  | 'delete:article'
-  | 'sync'
-  | 'version'
-  | 'help';
-
-class Main {
-  // command-line-usageがエラーが出て使えないので以下の実装
-  private readonly mainUsage: string = `Command:
-  qiita init                    qiitaとの接続設定. 初回のみ実行
-  qiita pull:article            既に投稿している記事をローカルにpull(強制上書き)
-  qiita new:article             新しい記事を追加
-  qiita post:article            ローカルで新規作成した記事を選択的に投稿
-  qiita patch:article           ローカルで修正した記事を選択的に投稿
-  qiita delete:article(未実装)   選択した記事の削除
-  qiita sync(未実装)             ローカルで作成/修正した記事の一括反映および投稿済み記事の取得
-  qiita --version, -v           qiita-cliのバージョンを表示
-  qiita --help, -h              ヘルプ
+const mainUsage: string = `Command:
+qiita init                    qiitaとの接続設定. 初回のみ実行
+qiita pull:article            既に投稿している記事をローカルにpull(強制上書き)
+qiita new:article             新しい記事を追加
+qiita post:article            ローカルで新規作成した記事を選択的に投稿
+qiita patch:article           ローカルで修正した記事を選択的に投稿
+qiita delete:article(未実装)   選択した記事の削除
+qiita sync(未実装)             ローカルで作成/修正した記事の一括反映および投稿済み記事の取得
+qiita --version, -v           qiita-cliのバージョンを表示
+qiita --help, -h              ヘルプ
 
 Remark:
-  コマンドは全て作業フォルダのルートでの実行を想定したものになっています.
-  記事の取得・投稿は作業フォルダはコマンド実行場所の作業フォルダ内のarticlesフォルダを基準に実行されます.
-  (articlesフォルダはqiita init や qiit pull コマンドで生成されます)
+コマンドは全て作業フォルダのルートでの実行を想定したものになっています.
+記事の取得・投稿は作業フォルダはコマンド実行場所の作業フォルダ内のarticlesフォルダを基準に実行されます.
+(articlesフォルダはqiita init や qiit pull コマンドで生成されます)
 `;
 
-  private commandMap = new Map<CommandType, () => number | Promise<number>>([
-    ['init', () => accessTokenInitialize()],
-    ['pull:article', () => pullArticle()],
-    ['new:article', () => newArticle()],
-    ['post:article', () => postArticle()],
-    ['patch:article', () => patchArticle()],
-    ['sync', () => new Calc().add(1, 2)],
-  ]);
+/**
+ * Set global CLI configurations
+ */
+program.storeOptionsAsProperties(false);
 
-  async run() {
-    // command-line-usageを利用しようとすると以下エラーを吐いてしまう
-    // TypeError: Cannot read property 'arrayify' of undefined
+program.version(
+  packageJson.version,
+  '-v, --version',
+  [
+    emoji.get('hatched_chick') + 'qiita cli',
+    'version:',
+    packageJson.version,
+  ].join(' ')
+);
 
-    console.log('\n' + emoji.get('hatched_chick') + ' qiita cli\n');
-    const exec = this.commandMap.get(process.argv[2]);
-    if (exec != null) {
-      const ret = await exec();
-      // TODO:Promise<numberの対応>
-      process.exit(ret);
-    } else {
-      if (process.argv[2] === '--help' || process.argv[2] === '-h') {
-        // help
-        console.log(this.mainUsage);
-      } else if (process.argv[2] === '--version' || process.argv[2] === '-v') {
-        // version
-        console.log('\n' + emoji.get('hatched_chick') + 'qiita cli');
-        console.log('version: ' + packageJson.version + '\n');
-      } else {
-        // other wrong args
-        console.log(
-          '\n' + emoji.get('disappointed') + ' wrong args\nfollow as below\n'
-        );
-        console.log(this.mainUsage);
-      }
-      process.exit(1);
-    }
-  }
-}
+program.usage(mainUsage);
 
-void new Main().run();
+program.helpOption('-h, --help', 'ヘルプ');
+
+program
+  .command('init')
+  .description('qiitaとの接続設定. 初回のみ実行')
+  .action(async () => {
+    await accessTokenInitialize();
+  });
+
+program
+  .command('pull:article')
+  .description('既に投稿している記事をローカルにpull(強制上書き)')
+  .action(async () => {
+    await pullArticle();
+  });
+
+program
+  .command('new:article')
+  .description('新しい記事を追加')
+  .action(async () => {
+    await newArticle();
+  });
+
+program
+  .command('post:article')
+  .description('ローカルで新規作成した記事を選択的に投稿')
+  .action(async () => {
+    await postArticle();
+  });
+
+program
+  .command('patch:article')
+  .description('ローカルで修正した記事を選択的に投稿')
+  .action(async () => {
+    await patchArticle();
+  });
+
+program
+  .command('sync')
+  .description('ローカルで作成/修正した記事の一括反映および投稿済み記事の取得')
+  .action(() => {
+    new Calc().add(1, 2);
+  });
+
+program.parse(process.argv);
