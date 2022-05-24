cf023342ee29fa30bc496e4246eec11910fcb020
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-05-25 03:29:03 +0900

    use fast-glob file search

--- src/commons/articlesDirectory.ts
+++ src/commons/articlesDirectory.ts
@@ -1,12 +1,6 @@
-import fs from 'fs';
 import path from 'path';
+import fg from 'fast-glob';
 
 export function loadArticleFiles(rootDir: string): string[] {
-  return fs
-    .readdirSync(rootDir, { withFileTypes: true })
-    .flatMap((dirent) =>
-      dirent.isFile()
-        ? [path.join(rootDir, dirent.name)]
-        : loadArticleFiles(path.join(rootDir, dirent.name))
-    );
+  return fg.sync(path.join(rootDir, '**', '*.md'), { dot: true });
 }
