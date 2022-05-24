59925cadcda4375ca705c6d1433612d448d1711e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-05-25 03:53:34 +0900

    globではpath.joinではなく'/'にしないと認識してくれなかったので対応

--- src/commons/articlesDirectory.ts
+++ src/commons/articlesDirectory.ts
@@ -1,6 +1,5 @@
-import path from 'path';
 import fg from 'fast-glob';
 
 export function loadArticleFiles(rootDir: string): string[] {
-  return fg.sync(path.join(rootDir, '**', '*.md'), { dot: true });
+  return fg.sync([rootDir, '**', '*.md'].join('/'), { dot: true });
 }
